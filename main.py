814ee2829bf3b325ad5ca8ddeb944bccb6d2145d
Yassin Sultan <96452651+YassinSultan@users.noreply.github.com>
2024-05-10 00:14:36 +0300

    final update in the code delete nose detection and use last version of face detection xml file

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import cv2
 import numpy as np
 
 # Load face and eye classifiers
-face_classifier = cv2.CascadeClassifier('./inputs/haarcascade_frontalface_default.xml')
+face_classifier = cv2.CascadeClassifier('./inputs/haarcascade_frontalface_alt2.xml')
 eye_classifier = cv2.CascadeClassifier('./inputs/haarcascade_eye_tree_eyeglasses.xml')
-nose_classifier = cv2.CascadeClassifier('./inputs/haarcascade_mcs_nose.xml')
 
 # Load the image
-image_path = "./inputs/images/person6.jpg"
+image_path = "./inputs/images/person7.jpg"
 image = cv2.imread(image_path)
 
 # Resize the image
@@ -30,33 +29,18 @@ for (fx, fy, fw, fh) in fake_faces:
 fake_eyes = eye_classifier.detectMultiScale(faked_gray_image)
 for (ex, ey, ew, eh) in fake_eyes:
     cv2.rectangle(faked_image, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
-
-# Apply nose detection to the fake image to see wrong results
-fake_nose = nose_classifier.detectMultiScale(faked_gray_image)
-for (nx, ny, nw, nh) in fake_nose:
-    cv2.rectangle(faked_image, (nx, ny), (nx + nw, ny + nh), (255, 255, 255), 2)
 # ---------------------------- Start -> Original Code -----------------------------------
 # Detect faces first
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-faces = face_classifier.detectMultiScale(gray_image)
+faces = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
 for (fx, fy, fw, fh) in faces:
-    # Detect eyes and nose within the face region
-    face_space = gray_image[fy:fy + fh, fx:fx + fw]
-    eyes = eye_classifier.detectMultiScale(face_space)
-    nose = nose_classifier.detectMultiScale(face_space)
-
-    # check if this face have eyes
-    if len(eyes) > 0:
-        # Draw a rectangle around the face
-        cv2.rectangle(image, (fx, fy), (fx + fw, fy + fh), (255, 0, 0), 2)
-
-    # draw the rectangle around the eyes
+    cv2.rectangle(image, (fx, fy), (fx + fw, fy + fh), (255, 0, 0), 2)
+    gray_face_space = gray_image[fy:fy + fh, fx:fx + fw]
+    face_space = image[fy:fy + fh, fx:fx + fw]
+    # Detect eyes within the face space
+    eyes = eye_classifier.detectMultiScale(gray_face_space)
     for (ex, ey, ew, eh) in eyes:
-        # draw the rectangle around the nose
-        for (nx, ny, nw, nh) in nose:
-            if ny > ey:
-                cv2.rectangle(image, (fx + ex, fy + ey), (fx + ex + ew, fy + ey + eh), (0, 0, 255), 2)
-                # cv2.rectangle(image, (x + nx, y + ny), (x + nx + nw, y + ny + nh), (255, 0, 255), 2)
+        cv2.rectangle(face_space, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
 
 
 # Display the result
